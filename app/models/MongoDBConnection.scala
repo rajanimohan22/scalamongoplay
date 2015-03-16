4ce2079b3b47b989c61f805f2a47e557fecaf7f1
Mohan Rao Dolla <raomohan2006@yahoo.com>
2015-03-16 16:07:17 +0000

    Update MongoDBConnection.scala
    Change of IP Address for MongoDB Host

--- app/models/MongoDBConnection.scala
+++ app/models/MongoDBConnection.scala
@@ -7,7 +7,7 @@ import com.mongodb.casbah.MongoClient
 import com.mongodb.casbah.MongoCollection
 object MongoDBConnection {
   def getConnection() : MongoCollection = {
-    val mongoClient =  MongoClient("192.168.1.5", 27017)
+    val mongoClient =  MongoClient("127.0.0.1", 27017)
     val db = mongoClient("discussionBoardDB")
     val collection = db("commentsData")
     collection
